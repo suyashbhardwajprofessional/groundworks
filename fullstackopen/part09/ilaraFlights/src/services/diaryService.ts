5c291d83d3290ebe9042a7dbd625bf7b4ef94eea
suyash bhardwaj <suyash_bhardwaj@rediffmail.com>
2025-03-31 15:21:43 +0530

    part 08 groundworks

---
+++ fullstackopen/part09/ilaraFlights/src/services/diaryService.ts
@@ -0,0 +1,47 @@
+import diaries from '../../data/entries';
+import { NonSensitiveDiaryEntry, DiaryEntry, NewDiaryEntry } from '../types';
+
+const getEntries = (): DiaryEntry[] => {
+  return diaries;
+};
+
+// const getNonSensitiveEntries = (): Pick<DiaryEntry, 'id' | 'date' | 'weather' | 'visibility'>[] => {
+//     return diaries;
+//   }
+// const getNonSensitiveEntries = (): Omit<DiaryEntry, 'comment'>[] => {
+//     return diaries;
+// }
+// const getNonSensitiveEntries = (): NonSensitiveDiaryEntry[] => {  
+//     return diaries;
+// };
+
+const findById = (id: number): DiaryEntry | undefined=> {
+  const entry = diaries.find(d => d.id === id);
+  return entry;
+};
+
+const getNonSensitiveEntries = (): NonSensitiveDiaryEntry[] => {
+    return diaries.map(({ id, date, weather, visibility }) => ({
+      id,
+      date,
+      weather,
+      visibility,
+    }));
+  };
+
+  const addDiary = ( entry: NewDiaryEntry ): DiaryEntry => {
+  const newDiaryEntry = {
+    id: Math.max(...diaries.map(d => d.id)) + 1,
+    ...entry
+  };
+
+  diaries.push(newDiaryEntry);
+  return newDiaryEntry;
+};
+
+export default {
+  getEntries,
+  addDiary,
+  getNonSensitiveEntries,
+  findById
+};
