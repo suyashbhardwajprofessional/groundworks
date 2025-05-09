fa599e128d139be061c46ecf61bec58aee71373b
suyashbhardwaj <suyash_bhardwaj@rediffmail.com>
2025-05-09 21:40:09 +0530

    made containedFE application containers communicate successfully

--- fullstackopen/part12/containedFE/vite.config.js
+++ fullstackopen/part12/containedFE/vite.config.js
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    host: '0.0.0.0',
+    strictPort: true,
+    port: 5173,
+    // Allow Docker internal hostnames like "app"
+    origin: 'http://app:5173',
+    hmr: {
+      host: 'localhost', // or your host IP if using HMR externally
+    },
+    headers: {
+      'Access-Control-Allow-Origin': '*', // optional
+    },
+  },
+});
